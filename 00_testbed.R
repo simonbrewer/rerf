bb2fd697185483793d3fa1f2a77af93c5f1bbea9
Simon Brewer <brewersi@gmail.com>
2023-01-25 09:36:35 -0700

    Working iteration loop

--- 00_testbed.R
+++ 00_testbed.R
@@ -1,6 +1,6 @@
 ## Testbed for function
 
-rerf <- function(X, y, re, data, 
+rerf <- function(X, y, re, data,
                  initre = 0, max_iter = 100, tol = 1e-5) {
   
   library(lme4)
@@ -9,13 +9,24 @@ rerf <- function(X, y, re, data,
   ## Initialize
   y_adj <- y - initre 
   error_tol <- 1e6
+  iter <- 1
   
   ## Model 1
   rf <- randomForest(X, y_adj)
   
   while(iter < max_iter & error_tol > tol) {
-    print(re)
+    print(paste(iter, re))
+    iter <- iter + 1
   }
   
   
 }
+
+sleep <- read.csv("data/sleepstudy.csv")
+
+X = data.frame(days = sleep$Days)
+y = sleep$Reaction
+
+re = "(1 | Subject)"
+
+rerf(X, y, re)
