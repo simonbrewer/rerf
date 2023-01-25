0b87021557ee0714f7630fc10975d4fda9d2b70a
Simon Brewer <brewersi@gmail.com>
2023-01-25 15:02:39 -0700

    Full EM loop added

--- 00_testbed.R
+++ 00_testbed.R
@@ -1,4 +1,5 @@
 ## Testbed for function
+set.seed(42)
 
 rerf <- function(X, y, re, data,
                  initre = 0, max_iter = 100, tol = 1e-5) {
@@ -8,25 +9,42 @@ rerf <- function(X, y, re, data,
   
   ## Initialize
   y_adj <- y - initre 
-  error_tol <- 1e6
+  error_tol <- 1e-1
   iter <- 1
   
   ## Model 1
-  rf <- randomForest(X, y_adj)
-  
   while(iter < max_iter & error_tol > tol) {
-    print(paste(iter, re))
+    
+    ## E-step
+    rf_mod <- randomForest(X, y_adj)
+    
+    yhat <- predict(rf_mod, X)
+    rf_resid <- y - yhat
+    rf_resid <- y - rf_mod$predicted
+    
+    re_formula <- as.formula(paste('rf_resid ~ -1 +', re))
+    re_mod <- lmer(re_formula, data = data)
+    print(paste(iter, logLik(re_mod)))
+    
+    ## M-step
+    re_hat <- predict(re_mod)
+    
+    #  y-Zb
+    y_adj <- y - re_hat
+    
+    ## Iterations
     iter <- iter + 1
   }
   
+  return(list(rf = rf_mod, lmer = re_mod, re = ranef(re_mod)))
   
 }
 
 sleep <- read.csv("data/sleepstudy.csv")
 
-X = data.frame(days = sleep$Days)
+X = data.frame(Days = sleep$Days)
 y = sleep$Reaction
 
-re = "(1 | Subject)"
+re = "(Days | Subject)"
 
-rerf(X, y, re)
+fit <- rerf(X, y, re, data = sleep, max_iter = 100)
